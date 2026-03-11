dde2eba0417750d6ca484914bddbe384717fcb6f
Sabarish <sabarishsanju@gmail.com>
2026-03-11 23:32:31 +0530

    last commit

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const connectDB = require("./config/db.js");
 const express = require("express");
-const userRoutes = require("./routes/userRoutes.js");
 const cors = require("cors");
+const userRoutes = require("./routes/userRoutes.js");
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
 connectDB();
+
 app.use("/api", userRoutes);
-app.listen(process.env.PORT, () => {
-  console.log("server is running on port", process.env.PORT);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
